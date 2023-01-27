bf989b9c0bda37cb12eeea827590fcb2c64797fa
lenaSamaha <lena.sama7a@gmail.com>
2023-01-27 21:37:21 +0200

    Editing Requirment 6

--- Requirement6.sql
+++ Requirement6.sql
@@ -1,7 +1,16 @@
-CREATE FUNCTION fnCompanionsList (@EpisodeId INT)
+CREATE OR ALTER FUNCTION fnCompanionsList (@EpisodeId INT)
 RETURNS Table As
 RETURN
 SELECT C.CompanionName FROM tblCompanion as C INNER JOIN tblEpisodeCompanion as EC ON  C.CompanionId = EC.CompanionId WHERE EpisodeId = @EpisodeId;
 
+CREATE OR ALTER FUNCTION fnCompanions(@EpisodeId INT)
+RETURNS VARCHAR(700) AS
+BEGIN 
+	DECLARE @Ret AS VARCHAR(700)
+	SELECT @Ret=STRING_AGG (CompanionName,', ') 
+	FROM fnCompanionsList(@EpisodeId);
+	RETURN @Ret
+END;
+
 --To Call it:
-SELECT * from [dbo].fnCompanionsList(2) ;
+SELECT [dbo].fnCompanions(2) ;
